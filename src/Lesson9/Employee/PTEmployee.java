c9ecccc0f2524fb8b0713740abbc3a0af4bc5509
jaso1990 <jaso1990@ISS2TZQKH2.scdsb.pri>
2019-11-04 10:32:44 -0500

    worked at school

---
+++ src/Lesson9/Employee/PTEmployee.java
@@ -0,0 +1,17 @@
+
+package Lesson9.Employee;
+
+
+public class PTEmployee extends Employee {
+    
+    public PTEmployee(){
+        super();
+    }
+
+    @Override
+    public double getPay() {
+        totalPay += rate * hours; //adding to total pay
+        return hours * rate;
+    }
+    
+}
